924799ed6bdf204b7e2626d7edafd245eeff29c1
Ko4ka <vlad.chor2019@gmail.com>
2026-09-17 00:08:48 +0300

    Enhance output messages for distance calculations

--- lab1/lab44v.py
+++ lab1/lab44v.py
@@ -1,7 +1,9 @@
 speed = int(input("Введіть швидкість: "))
+
 distance_6 = speed * 6
 distance_10 = speed * 10
 distance_15 = speed * 15
-print(distance_6)
-print(distance_10)
-print(distance_15)
+
+print(f"Відстань, яку подолає автомобіль за 6 годин: {distance_6}")
+print(f"Відстань, яку подолає автомобіль за 10 годин: {distance_10}")
+print(f"Відстань, яку подолає автомобіль за 15 годин: {distance_15}")
